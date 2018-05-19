e7b0a7218a2d774aafa79d66d8d68ffefbe78601
alex.swansboro@gmail.com <alex.swansboro@gmail.com>
2018-05-19 22:36:22 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,30 @@
+function add (a, b) {
+  return (a + b)
+}
+function subtract (a, b) {
+  return (a - b)
+}
+function multiply (a, b) {
+  return (a * b)
+}
+function divide (a, b) {
+  return (a / b)
+}
+function inc (a) {
+  return (a + 1)
+}
+function dec (a) {
+  return (a - 1)
+}
+function makeInt(string) {
+ return (string)
+}
+function parseInt (0*2328, 10) {
+  return (0)
+}
+parseInt ('sldkjflksjf') {
+  return ('isNaN')
+}
+
+ 
+
